873f8e9b8fd1ef57e66768479e4828b27b1bdd2c
NKCLIII <nikkrsic@hotmail.com>
2020-06-08 14:22:45 +0000

    Second commit.

---
+++ src/test/java/nkmg1c/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nkmg1c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
